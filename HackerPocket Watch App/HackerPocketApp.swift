acbcd4d53ce87d27fb43eb87e3b78e9f49d9ca38
Dakota Kim <codingvampire@gmail.com>
2024-03-23 13:51:54 -0400

    Initial Commit

---
+++ HackerPocket Watch App/HackerPocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerPocketApp.swift
+//  HackerPocket Watch App
+//
+//  Created by Dakota Kim on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerPocket_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
