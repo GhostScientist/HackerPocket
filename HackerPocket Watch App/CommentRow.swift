43432c0f5e5535716eb1178f70c7befbd8b4d229
Dakota Kim <codingvampire@gmail.com>
2024-03-29 23:20:49 -0400

    add tap to expand for comments

--- HackerPocket Watch App/CommentRow.swift
+++ HackerPocket Watch App/CommentRow.swift
@@ -9,20 +9,28 @@ import SwiftUI
 
 struct CommentRow: View {
     var comment: Comment
+    @State private var isExpanded: Bool = false
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 5) {
-            Text(comment.by)
-                .font(.headline)
-            Text(comment.textWithoutTags)
-                .font(.footnote)
-                .lineLimit(4)
-                .truncationMode(.tail)
-            Text(.init(comment.postedTime))
-                .font(.footnote)
+        VStack(alignment: .leading) {
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(comment.by)
+                    .font(.headline)
+                Text(comment.textWithoutTags)
+                    .font(.footnote)
+                    .lineLimit(isExpanded ? nil : 2)
+                    .truncationMode(.tail)
+                Text(.init(comment.postedTime))
+                    .font(.footnote)
+            }
         }
+        .onTapGesture {
+            isExpanded.toggle()        }
     }
 }
 
+
 #Preview {
-    CommentRow(comment: Comment(id: 1, by: "TestComment", text: "This is a test comment!", time: Int(Date.timeIntervalSinceReferenceDate.magnitude), type: "story"))
+    CommentRow(comment: Comment(id: 1, by: "Zt", text: "This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment! This is a test comment!", time: Int(Date.timeIntervalSinceReferenceDate.magnitude), type: "story"))
 }
