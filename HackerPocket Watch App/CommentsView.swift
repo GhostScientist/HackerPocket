72d853a1e0c59d749fab29199ecd2891ec1c76c6
Dakota Kim <codingvampire@gmail.com>
2024-03-29 23:43:12 -0400

    show tip for tap to expand

--- HackerPocket Watch App/CommentsView.swift
+++ HackerPocket Watch App/CommentsView.swift
@@ -7,10 +7,29 @@
 
 import SwiftUI
 
+struct CustomTipView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "info.circle")
+                .foregroundColor(.blue)
+            Text("Tap on a comment to expand it.")
+                .font(.caption)
+        }
+        .padding(8)
+        .background(Color(.lightGray))
+        .cornerRadius(8)
+        .shadow(radius: 4)
+    }
+}
+
 struct CommentsView: View {
     let commentIds: [Int]
     @State private var comments: [Comment] = []
-
+    @State private var isLoading = true
+    @State private var showTip = false
+    
+    @AppStorage("commentsTipShown") private var commentsTipShown = false
+    
     var body: some View {
         List(comments) { comment in
             CommentRow(comment: comment)
@@ -19,43 +38,50 @@ struct CommentsView: View {
         .onAppear {
             fetchComments()
         }
+        .onChange(of: isLoading) { oldValue, newValue in
+            if !newValue && !showTip && !commentsTipShown {
+                showTip = true
+                commentsTipShown = true
+            }
+        }
+        .overlay(
+            VStack {
+                if showTip {
+                    CustomTipView()
+                        .padding(.top, 8)
+                        .transition(.opacity.combined(with: .move(edge: .top)))
+                        .onTapGesture {
+                            withAnimation {
+                                showTip = false
+                            }
+                        }
+                }
+                Spacer()
+            }
+        )
     }
-
+    
     func fetchComments() {
+        isLoading = true
+        var fetchedComments: [Comment] = []
+        
+        let group = DispatchGroup()
         for commentId in commentIds {
+            group.enter()
             let url = URL(string: "https://hacker-news.firebaseio.com/v0/item/\(commentId).json")!
             URLSession.shared.dataTask(with: url) { data, response, error in
                 if let data = data {
                     if let comment = try? JSONDecoder().decode(Comment.self, from: data) {
-                        DispatchQueue.main.async {
-                            print(comment)
-                            comments.append(comment)
-                        }
+                        fetchedComments.append(comment)
                     }
                 }
+                group.leave()
             }.resume()
         }
-    }
-}
-
-extension String {
-    var decodingUnicodeCharacters: String { applyingTransform(.init("Hex-Any"), reverse: false) ?? "" }
-}
-
-extension String {
-    func unescape() -> String {
-        let characters = [
-            "&amp;": "&",
-            "&lt;": "<",
-            "&gt;": ">",
-            "&quot;": "\"",
-            "&apos;": "'"
-        ]
-        var str = self
-        for (escaped, unescaped) in characters {
-            str = str.replacingOccurrences(of: escaped, with: unescaped, options: NSString.CompareOptions.literal, range: nil)
+        
+        group.notify(queue: .main) {
+            comments = fetchedComments.sorted { $0.time > $1.time }
+            isLoading = false
         }
-        return str
     }
 }
-
